60986ed104d127f035898a8677c518ae82559282
Lucas Viana <lucasg.viana@outlook.com>
2021-09-25 01:13:43 -0300

    Otimizando a modelagem via contexto

--- Fornecedores/src/LV.Infra/Data/Context/MeuDbContext.cs
+++ Fornecedores/src/LV.Infra/Data/Context/MeuDbContext.cs
@@ -2,13 +2,17 @@
 using LV.Buniness.Models.Produtos;
 using LV.Infra.Data.Mappings;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace LV.Infra.Data.Context
 {
     class MeuDbContext : DbContext
     {
         public MeuDbContext() : base(nameOrConnectionString: "DefaultConnection") 
-        { }
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
@@ -16,9 +20,20 @@ namespace LV.Infra.Data.Context
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p
+                    .HasColumnType("varchar")
+                    .HasMaxLength(10));
+
             modelBuilder.Configurations.Add(new FornecedorConfig());
             modelBuilder.Configurations.Add(new EnderecoConfig());
             modelBuilder.Configurations.Add(new ProdutoConfig());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- Fornecedores/src/LV.Infra/Migrations/Configuration.cs
+++ Fornecedores/src/LV.Infra/Migrations/Configuration.cs
@@ -6,7 +6,7 @@ namespace LV.Infra.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
     }
 }
